b4a1a915393dcb05b92bdd1af4d64b06ad9eed91
Елисей Чубукин <e-chubukin2017@yandex.ru>
2025-03-01 13:20:58 +0700

    Project Structure

--- django2025/django2025/urls.py
+++ django2025/django2025/urls.py
@@ -9,4 +9,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name ='index'),
     path('about/', views.about, name ='about'),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
