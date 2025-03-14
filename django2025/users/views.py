a0d6ac6d10679e68a44d4ae9f0a37af10bc071b0
Mobostrayk <e-chubukin2017@yandex.ru>
2025-03-15 04:16:57 +0700

    Профиль

--- django2025/users/views.py
+++ django2025/users/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import login, authenticate
 from .forms import RegisterForm, LoginForm
+from .models import Profile
 
 def registration(request):
     if request.method == 'POST':
@@ -11,7 +13,7 @@ def registration(request):
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
             login(request, user)
-            return redirect('index')
+            return redirect('profile')
     else:
         form = RegisterForm()
     return render(request, 'users/registration.html', {'form': form})
@@ -25,10 +27,12 @@ def user_login(request):
             user = authenticate(username=username, password=password)
             if user is not None:
                 login(request, user)
-                return redirect('index')
+                return redirect('profile')
     else:
         form = LoginForm()
     return render(request, 'users/login.html', {'form': form})
 
+@login_required
 def profile(request):
-    return render(request, 'profile/.html', )
+    user_profile = get_object_or_404(Profile, user=request.user)
+    return render(request, 'users/profile.html', {'profile': user_profile})
